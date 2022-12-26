4f2907f433f567ccb0677a47ed879a37df3a388f
Saransh Mehta <saransh@jify.co>
2022-12-26 11:49:58 -0700

    Change backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,159 +1,177 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import AppHeader from "./components/AppHeader/AppHeader";
 import Form from "./components/Landing/Form/Form";
 import SessionContext from "./store/session-context";
-import axios from "axios"
+import axios from "axios";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Error from "./components/Error/Error";
 import Overlay from "./components/Dashboard/Overlay/Overlay";
 import Loader from "./components/Loader/Loader";
 
 function App() {
-
-  const [ appState, setAppState ] = useState({
+  const [appState, setAppState] = useState({
     loggedIn: false,
     page: "landing",
     userDetails: {
       email: "",
       name: "",
       token: "",
-      _id: ""
+      _id: "",
     },
     overlay: false,
     error: {
       errorState: false,
-      errorMsg: ""
+      errorMsg: "",
     },
     movieModal: {
       movieModalState: false,
-      movieId: ""
-    }
-  })
-  const [ loading, setLoading ] = useState(false)
+      movieId: "",
+    },
+  });
+  const [loading, setLoading] = useState(false);
 
   useEffect(async () => {
     const reqData = {
       _id: localStorage["movie_app_id"],
-      token: localStorage["movie_app_token"]
-    }
+      token: localStorage["movie_app_token"],
+    };
     try {
-      setLoading(true)
-      const { data } = await axios.post("http://localhost:9000/auto_login", reqData)
-      setLoading(false)
-      console.log(data)
-      if(!data.error) {
-        setAppState((prevState) => { 
+      setLoading(true);
+      const { data } = await axios.post(
+        "https://mern-movie-app.onrender.com/auto_login",
+        reqData
+      );
+      setLoading(false);
+      console.log(data);
+      if (!data.error) {
+        setAppState((prevState) => {
           return {
             ...prevState,
             userDetails: {
               email: data.data.email,
               name: data.data.name,
               token: data.data.token,
-              _id: data.data._id
+              _id: data.data._id,
             },
             loggedIn: true,
-            page: "dashboard"
-          } 
-        })
+            page: "dashboard",
+          };
+        });
+      } else if (data.error) {
+        throw Error("Something went wrong");
       }
-      else if(data.error) {
-        throw Error("Something went wrong")
-      }
+    } catch (err) {
+      console.log(err);
     }
-    catch(err) {
-      console.log(err)
-    }
-  }, [])
+  }, []);
 
   const loginSignupHandler = async (userObj) => {
-    setAppState((prevState) => { 
+    setAppState((prevState) => {
       return {
         ...prevState,
         userDetails: {
           _id: userObj._id,
           email: userObj.email,
           name: userObj.name,
-          token: userObj.token
+          token: userObj.token,
         },
         loggedIn: true,
-        page: "dashboard"
-      } 
-    })
-    localStorage["movie_app_id"] = userObj._id
-    localStorage["movie_app_token"] = userObj.token
-    console.log(userObj)
-  }
+        page: "dashboard",
+      };
+    });
+    localStorage["movie_app_id"] = userObj._id;
+    localStorage["movie_app_token"] = userObj.token;
+    console.log(userObj);
+  };
 
   const logoutHandler = async () => {
     try {
       const reqData = {
-          _id: appState.userDetails._id,
-          token: appState.userDetails.token
-      }
-      const resp = await axios.post("http://localhost:9000/logout", reqData)
-      console.log(resp.data.data)
-      setAppState((prevState) => { 
+        _id: appState.userDetails._id,
+        token: appState.userDetails.token,
+      };
+      const resp = await axios.post(
+        "https://mern-movie-app.onrender.com/logout",
+        reqData
+      );
+      console.log(resp.data.data);
+      setAppState((prevState) => {
         return {
           ...prevState,
           userDetails: {
             email: "",
             name: "",
             token: "",
-            _id: ""
+            _id: "",
           },
           loggedIn: false,
-          page: "landing"
-        } 
-      })
-      localStorage["movie_app_id"] = ""
-      localStorage["token"] = ""
+          page: "landing",
+        };
+      });
+      localStorage["movie_app_id"] = "";
+      localStorage["token"] = "";
+    } catch (err) {
+      console.log(err);
     }
-    catch (err) {
-        console.log(err)
-    }
-  }
+  };
 
   const starredHandler = () => {
-    setAppState((prevState) => { 
+    setAppState((prevState) => {
       return {
         ...prevState,
-        page: "starred"
-      } 
-    })
-  }
+        page: "starred",
+      };
+    });
+  };
 
   const closeOverlayHandler = () => {
     setAppState((prevState) => {
-        return {
-            ...prevState,
-            overlay: false,
-            error: {
-                errorState: false,
-                errorMsg: ""
-            },
-            movieModal: {
-                movieModalState: false,
-                movieId: ""
-            }
-        }
-    })
-}
+      return {
+        ...prevState,
+        overlay: false,
+        error: {
+          errorState: false,
+          errorMsg: "",
+        },
+        movieModal: {
+          movieModalState: false,
+          movieId: "",
+        },
+      };
+    });
+  };
 
   return (
-      <SessionContext.Provider value = {{
+    <SessionContext.Provider
+      value={{
         appState: appState,
         setAppState: setAppState,
-        logoutHandler: logoutHandler
-      }}>
+        logoutHandler: logoutHandler,
+      }}
+    >
       {console.log("ap state", appState)}
       {appState.error.errorState && (
-        <Overlay onCloseOverlay = {closeOverlayHandler} onUnStar = {() => {}}/>
+        <Overlay onCloseOverlay={closeOverlayHandler} onUnStar={() => {}} />
       )}
-      {loading ? <Loader /> : (
-        <div className = "app">
-          <AppHeader loggedIn = {appState.loggedIn} page = {appState.page} onStarred = {starredHandler}/>
-          {!appState.loggedIn && (appState.page == "landing") && <Form onLoginSignup = {loginSignupHandler}/>}
-          {appState.loggedIn && (appState.page == "dashboard" || appState.page == "genre" || appState.page == "search" || appState.page == "starred") && <Dashboard starred = {appState.page == "starred" ? true : false}/>}
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="app">
+          <AppHeader
+            loggedIn={appState.loggedIn}
+            page={appState.page}
+            onStarred={starredHandler}
+          />
+          {!appState.loggedIn && appState.page == "landing" && (
+            <Form onLoginSignup={loginSignupHandler} />
+          )}
+          {appState.loggedIn &&
+            (appState.page == "dashboard" ||
+              appState.page == "genre" ||
+              appState.page == "search" ||
+              appState.page == "starred") && (
+              <Dashboard starred={appState.page == "starred" ? true : false} />
+            )}
         </div>
       )}
     </SessionContext.Provider>
--- frontend/src/components/Dashboard/MovieModal/MovieModal.js
+++ frontend/src/components/Dashboard/MovieModal/MovieModal.js
@@ -1,131 +1,158 @@
-import React, { useContext, useEffect, useState, useCallback } from "react"
-import './MovieModal.css'
-import axios from "axios"
-import SessionContext from "../../../store/session-context"
-import Loader from "../../Loader/Loader"
+import React, { useContext, useEffect, useState, useCallback } from "react";
+import "./MovieModal.css";
+import axios from "axios";
+import SessionContext from "../../../store/session-context";
+import Loader from "../../Loader/Loader";
 
 const MovieModal = (props) => {
+  const ctx = useContext(SessionContext);
+  const [movieState, setMovieState] = useState({
+    id: "",
+    title: "Title",
+    release_date: "00-00-00",
+    vote_average: 0,
+    vote_count: 0,
+    overview: "Dummy Text",
+    poster: "",
+  });
+  const [starred, setStarred] = useState(false);
+  const [modalError, setModalError] = useState({
+    errorState: false,
+    errorMsg: "",
+  });
+  const [modalLoader, setModalLoader] = useState(false);
 
-    const ctx = useContext(SessionContext)
-    const [ movieState, setMovieState ] = useState({
-        id: "",
-        title: "Title",
-        release_date: "00-00-00",
-        vote_average: 0,
-        vote_count: 0,
-        overview: "Dummy Text",
-        poster: ""
-    })
-    const [ starred, setStarred ] = useState(false)
-    const [ modalError, setModalError ] = useState({
-        errorState: false,
-        errorMsg: ""
-    })
-    const [ modalLoader, setModalLoader ] = useState(false)
-
-    const getMovie = useCallback(async () => {
-        setModalLoader(true)
-        const movieResponse = await axios.post("http://localhost:9000/movie", { _id: ctx.appState.userDetails._id, id: ctx.appState.movieModal.movieId }, { 
-            headers: {
-                "_id": ctx.appState.userDetails._id, 
-                "token": ctx.appState.userDetails.token 
-            }
-        })
-        setModalLoader(false)
-        if(movieResponse.data.error) {
-            if(movieResponse.data.msg == "Can't fetch data") {
-                setModalError({
-                    errorState: true,
-                    errorMsg: "Unable to fetch data right now. Please try again later."
-                })
-                return
-            }
-            else {
-                setModalError({
-                    errorState: true,
-                    errorMsg: "Something went wrong, please refresh."
-                })
-                return
-            }
-        }
-        const { movie } = movieResponse.data.data
-        console.log("movie", movie)
-        setMovieState({
-            id: movie.id,
-            title: movie.title,
-            release_date: movie.release_date,
-            vote_average: movie.vote_average,
-            vote_count: movie.vote_count,
-            overview: movie.overview,
-            poster: movie.poster
-        })
-        setStarred(movie.starred)
-    }, [])
-
-    const starClickHandler = async () => {
-        if(!starred) {
-            const starResponse = await axios.post("http://localhost:9000/star", { _id: ctx.appState.userDetails._id, id: movieState.id }, { 
-                headers: {
-                    "_id": ctx.appState.userDetails._id, 
-                    "token": ctx.appState.userDetails.token 
-                }
-            })
-            if(starResponse.data.error) {
-                return
-            }
-            setStarred(true)
-        }
-        else if(starred) {
-            const starResponse = await axios.post("http://localhost:9000/unstar", { _id: ctx.appState.userDetails._id, id: movieState.id }, { 
-                headers: {
-                    "_id": ctx.appState.userDetails._id, 
-                    "token": ctx.appState.userDetails.token 
-                }
-            })
-            if(starResponse.data.error) {
-                return
-            }
-            props.onUnStar(movieState.id)
-            setStarred(false)
-        }
+  const getMovie = useCallback(async () => {
+    setModalLoader(true);
+    const movieResponse = await axios.post(
+      "https://mern-movie-app.onrender.com/movie",
+      {
+        _id: ctx.appState.userDetails._id,
+        id: ctx.appState.movieModal.movieId,
+      },
+      {
+        headers: {
+          _id: ctx.appState.userDetails._id,
+          token: ctx.appState.userDetails.token,
+        },
+      }
+    );
+    setModalLoader(false);
+    if (movieResponse.data.error) {
+      if (movieResponse.data.msg == "Can't fetch data") {
+        setModalError({
+          errorState: true,
+          errorMsg: "Unable to fetch data right now. Please try again later.",
+        });
+        return;
+      } else {
+        setModalError({
+          errorState: true,
+          errorMsg: "Something went wrong, please refresh.",
+        });
+        return;
+      }
     }
+    const { movie } = movieResponse.data.data;
+    console.log("movie", movie);
+    setMovieState({
+      id: movie.id,
+      title: movie.title,
+      release_date: movie.release_date,
+      vote_average: movie.vote_average,
+      vote_count: movie.vote_count,
+      overview: movie.overview,
+      poster: movie.poster,
+    });
+    setStarred(movie.starred);
+  }, []);
 
-    useEffect(() => {
-        getMovie()
-    }, [])
+  const starClickHandler = async () => {
+    if (!starred) {
+      const starResponse = await axios.post(
+        "https://mern-movie-app.onrender.com/star",
+        { _id: ctx.appState.userDetails._id, id: movieState.id },
+        {
+          headers: {
+            _id: ctx.appState.userDetails._id,
+            token: ctx.appState.userDetails.token,
+          },
+        }
+      );
+      if (starResponse.data.error) {
+        return;
+      }
+      setStarred(true);
+    } else if (starred) {
+      const starResponse = await axios.post(
+        "https://mern-movie-app.onrender.com/unstar",
+        { _id: ctx.appState.userDetails._id, id: movieState.id },
+        {
+          headers: {
+            _id: ctx.appState.userDetails._id,
+            token: ctx.appState.userDetails.token,
+          },
+        }
+      );
+      if (starResponse.data.error) {
+        return;
+      }
+      props.onUnStar(movieState.id);
+      setStarred(false);
+    }
+  };
 
-    return (
-        <div className = "movie-modal">
-            {modalLoader && <Loader />}
-            {modalError.errorState && (
-                <div className = "movie-modal__error">
-                    <h4>{modalError.errorMsg}</h4>
-                </div>
-            )}
-            {!modalError.errorState && !modalLoader && (
-                <React.Fragment>
-                    <div className = "poster">
-                        <img src = {!movieState.poster || movieState.poster == "" ? "https://img-premium.flaticon.com/png/512/1823/premium/1823458.png?token=exp=1632477527~hmac=7bfcd24f58a0f9b1283b10187cc522e8" : movieState.poster} className = "poster-img" />
-                    </div>
-                    <div className = "movie-details">
-                        <p className = "movie-details-title">{movieState.title}</p>
-                        <hr className = "line-title"/>
-                        <div className = "movie-details-top">
-                            <div className = "movie-details-info">
-                                <p className = "movie-details-date">{movieState.release_date}</p>
-                                <p className = "movie-details-avg"><span>{movieState.vote_average}</span>&nbsp;/&nbsp;10 || {movieState.vote_count} votes</p>
-                            </div>
-                            <div className = {`star ${starred ? "starred" : "unstarred"}`} onClick = {starClickHandler}></div>
-                        </div>
-                        <hr className = "line-top"/>
-                        <div className = "movie-details-overview">
-                            <p className = "movie-details-overview-text">{movieState.overview}</p>
-                        </div>
-                    </div>
-                </React.Fragment>
-            )}
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div className="movie-modal">
+      {modalLoader && <Loader />}
+      {modalError.errorState && (
+        <div className="movie-modal__error">
+          <h4>{modalError.errorMsg}</h4>
         </div>
-    )
-}
+      )}
+      {!modalError.errorState && !modalLoader && (
+        <React.Fragment>
+          <div className="poster">
+            <img
+              src={
+                !movieState.poster || movieState.poster == ""
+                  ? "https://img-premium.flaticon.com/png/512/1823/premium/1823458.png?token=exp=1632477527~hmac=7bfcd24f58a0f9b1283b10187cc522e8"
+                  : movieState.poster
+              }
+              className="poster-img"
+            />
+          </div>
+          <div className="movie-details">
+            <p className="movie-details-title">{movieState.title}</p>
+            <hr className="line-title" />
+            <div className="movie-details-top">
+              <div className="movie-details-info">
+                <p className="movie-details-date">{movieState.release_date}</p>
+                <p className="movie-details-avg">
+                  <span>{movieState.vote_average}</span>&nbsp;/&nbsp;10 ||{" "}
+                  {movieState.vote_count} votes
+                </p>
+              </div>
+              <div
+                className={`star ${starred ? "starred" : "unstarred"}`}
+                onClick={starClickHandler}
+              ></div>
+            </div>
+            <hr className="line-top" />
+            <div className="movie-details-overview">
+              <p className="movie-details-overview-text">
+                {movieState.overview}
+              </p>
+            </div>
+          </div>
+        </React.Fragment>
+      )}
+    </div>
+  );
+};
 
-export default MovieModal
+export default MovieModal;
